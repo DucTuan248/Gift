972701dae89a5157d878e04f74a0e0134d763dc8
DucTuan248 <99781068+DucTuan248@users.noreply.github.com>
2023-10-20 13:58:17 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Những bông hoa đẹp nhất của lớp '
+const giftUrl = 'https://www.facebook.com/profile.php?id=100034705569556&mibextid=ZbWKwL';
+const eventName = "Happy Women's Day ";
+const titleCard = 'chú mừng và chúc mừng ';
+const contentCard = 'Nhân Ngày Phụ nữ Việt Nam, em chúc cô,các chị, các bạn nữ luôn tươi cười như hít phải khí N2O, bay bổng như H2, thăng hoa như I2, bản lĩnh như N2, và hiền hòa như H2O.Thành công với tất cả mục tiêu đặt ra ❤️❤️❤️'
+const giftImage = 'loptoi.png';
+const base64 = 'happy.png';
+const giftImageBase64 = "data:image/png;base64, " + base64;
